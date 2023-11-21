fe0fea2c88b099060ed17983d1e908d4110733c3
Juul Crienen <juulcrienen@gmail.com>
2023-11-21 13:36:41 +0100

    Added list handler

--- src/main/java/com/juulcrienen/githubapiwrapper/GitHubAPIWrapper.java
+++ src/main/java/com/juulcrienen/githubapiwrapper/GitHubAPIWrapper.java
@@ -4,6 +4,8 @@ import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GitHubAPIWrapper {
 
@@ -19,6 +21,14 @@ public class GitHubAPIWrapper {
 
     public GHRepository getGitHubRepository(String repository) throws IOException {
         return github.getRepository(repository);
+    }
+
+    public List<GHRepository> getGitHubRepositories(List<String> repositories) throws IOException{
+        List<GHRepository> result = new ArrayList<>();
+        for(String repo : repositories) {
+            result.add(github.getRepository(repo));
+        }
+        return result;
     }
 
 }
